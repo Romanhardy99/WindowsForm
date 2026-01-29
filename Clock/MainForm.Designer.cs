03acbd0c0572fbdaa92c1235ffbfc2fd2437e0d8
Romanhardy99 <144020402+Romanhardy99@users.noreply.github.com>
2026-01-29 20:39:04 +0400

    WindowsForm add project

---
+++ Clock/MainForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Clock
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.checkBoxShowDate = new System.Windows.Forms.CheckBox();
+            this.checkBoxShowWeekDay = new System.Windows.Forms.CheckBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F);
+            this.labelTime.Location = new System.Drawing.Point(12, 9);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(261, 51);
+            this.labelTime.TabIndex = 0;
+            this.labelTime.Text = "CurrentTime";
+            // 
+            // checkBoxShowDate
+            // 
+            this.checkBoxShowDate.AutoSize = true;
+            this.checkBoxShowDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.checkBoxShowDate.Location = new System.Drawing.Point(21, 184);
+            this.checkBoxShowDate.Name = "checkBoxShowDate";
+            this.checkBoxShowDate.Size = new System.Drawing.Size(132, 30);
+            this.checkBoxShowDate.TabIndex = 1;
+            this.checkBoxShowDate.Text = "ShowDate";
+            this.checkBoxShowDate.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxShowWeekDay
+            // 
+            this.checkBoxShowWeekDay.AutoSize = true;
+            this.checkBoxShowWeekDay.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.checkBoxShowWeekDay.Location = new System.Drawing.Point(21, 220);
+            this.checkBoxShowWeekDay.Name = "checkBoxShowWeekDay";
+            this.checkBoxShowWeekDay.Size = new System.Drawing.Size(181, 30);
+            this.checkBoxShowWeekDay.TabIndex = 2;
+            this.checkBoxShowWeekDay.Text = "ShowWeekDay";
+            this.checkBoxShowWeekDay.UseVisualStyleBackColor = true;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 259);
+            this.Controls.Add(this.checkBoxShowWeekDay);
+            this.Controls.Add(this.checkBoxShowDate);
+            this.Controls.Add(this.labelTime);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Clock";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.CheckBox checkBoxShowDate;
+        private System.Windows.Forms.CheckBox checkBoxShowWeekDay;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Clock/MainForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            this.Location = new Point
+                (
+                    Screen.PrimaryScreen.Bounds.Width - this.Width - 50, 50
+                );
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            labelTime.Text = DateTime.Now.ToString(
+                "hh:mm.ss tt",
+                System.Globalization.CultureInfo.InvariantCulture
+                );
+            if (checkBoxShowDate.Checked) 
+            {
+               labelTime.Text += $"\n{DateTime.Now.ToString("yyyy.MM.dd")}";
+            }
+            if (checkBoxShowWeekDay.Checked)
+            {
+                labelTime.Text += $"\n{DateTime.Now.DayOfWeek}";
+            }
+
+        }
+    }
+}
